9cb99d640b1242e091a61e1da71e7b174680d88e
WSH <864849414@qq.com>
2022-01-17 14:42:03 +0800

    提交代码

--- lib/widgets/SPClassToolBar.dart
+++ lib/widgets/SPClassToolBar.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sport/utils/SPClassCommonMethods.dart';
+import 'package:sport/utils/colors.dart';
 
 class SPClassToolBar extends AppBar{
   SPClassToolBar(
       BuildContext context,
       {String title,
-       Color spProBgColor:Colors.white,
-       int iconColor:0xFF333333,
+       Color spProBgColor:MyColors.main1,
+       int iconColor:0xFFFFFFFF,
         List<Widget> actions,
         bool showLead:true
       }
